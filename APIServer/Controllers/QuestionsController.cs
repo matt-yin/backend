754187831965f06498b17c2046a19657975b7174
Xiangrui Yin <yin.xiangrui@outlook.com>
2023-02-12 20:39:00 -0500

    Implemented GetQuestions API

---
+++ APIServer/Controllers/QuestionsController.cs
@@ -0,0 +1,33 @@
+﻿using DataAccess;
+using DataAccess.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionsController : ControllerBase
+    {
+        private readonly IDataRepository _dataRepository;
+
+        public QuestionsController(IDataRepository dataRepository)
+        {
+            _dataRepository = dataRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<Question> GetQuestions(string? search)
+        {
+            IEnumerable<Question> questions;
+            if (string.IsNullOrEmpty(search))
+            {
+                questions = _dataRepository.GetQuestions();
+            }
+            else
+            {
+                questions = _dataRepository.GetQuestionsBySearch(search);
+            }
+            return questions;
+        }
+    }
+}
--- APIServer/Program.cs
+++ APIServer/Program.cs
@@ -1,3 +1,5 @@
+using DataAccess;
+
 namespace APIServer
 {
     public class Program
@@ -7,7 +9,7 @@ namespace APIServer
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+            builder.Services.AddScoped<IDataRepository, DataRepository>();
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
